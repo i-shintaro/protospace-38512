7374bd9f62227a391ffa3a657b4746410975766d
i-shintaro <shintaroiwashita7857@gmail.com>
2024-06-04 14:07:15 +0900

    ProtoSpaceのプロトタイプ関連を実装しよう

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,2 +1,11 @@
 class Prototype < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :cath_copy, presence: true
+  validates :concept, presence: true
+  validates :user, presence: true
+  validates :image, presence: true
+
 end
